61aa82766568d39a02143b41af07d6cbe131f62b
Aed0-623P <aedo.felipe95@gmail.com>
2024-05-21 21:03:22 -0400

    wip render

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -30,9 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inmobiliario.onrender.com']
+ALLOWED_HOSTS = ['inmobiliario.onrender.com', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://inmobiliario.onrender.com']
-
 
 # Application definition
 
